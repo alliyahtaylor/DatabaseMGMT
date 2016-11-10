5270a7dee91502d1af8c7ab9f6e7612f4031f3af
theweirdskittle <serenatskino6@gmail.com>
2016-11-09 21:00:34 -0500

    Upload Lab8 Database SQL
    Alliyah Taylor
    Lab 8 
    11/9/2016

---
+++ Lab8_Database.sql
@@ -0,0 +1,76 @@
+--Alliyah Taylor
+--Lab 8
+--11/9/2016
+DROP TABLE IF EXISTS People;
+DROP TABLE IF EXISTS Actors;
+DROP TABLE IF EXISTS Directors;
+DROP TABLE IF EXISTS Directed;
+DROP TABLE IF EXISTS Acts_In;
+DROP TABLE IF EXISTS Movies;
+
+CREATE TABLE People(
+       PID 				INT NOT NULL,
+       First_Name 		TEXT NOT NULL,
+	   Last_Name 		TEXT NOT NULL,
+       AGE  			INT NOT NULL,
+       ADDRESS 			TEXT ,       
+       PRIMARY KEY (PID)
+);
+CREATE TABLE Actors(
+		AID 			INT NOT NULL,
+		PID 			INT NOT NULL references People(PID),
+		Birth_Date 		DATE,
+		Hair_Color		TEXT,
+		Height_Inches 	INT,
+		Weight			INT,
+		Favorite_Color	TEXT,
+		SAG_Anniversary DATE,
+		PRIMARY KEY (AID)
+);
+CREATE TABLE Directors(
+		DID 			INT NOT NULL,
+		PID 			INT NOT NULL references People(PID),
+		Film_School		TEXT,
+		Lens_Maker		TEXT,
+		DG_Anniversary 	DATE,
+		PRIMARY KEY (DID)
+);
+CREATE TABLE Movies(
+		MPAA_Num 			INT NOT NULL,
+		Name				TEXT NOT NULL,
+		Year_Released		DATE NOT NULL,
+		Domestic_BO_Sales	INT,
+		Foreign_BO_Sales 	INT,
+		DVD_BluRay_Sales 	INT,
+		PRIMARY KEY (MPAA_Num)
+);
+CREATE TABLE Acts_In(
+		AID 			INT NOT NULL references Actors(AID),
+		MPAA_Num		INT NOT NULL references Movies(MPAA_Num),
+		PRIMARY KEY (AID, MPAA_Num)
+);
+CREATE TABLE Directed(
+		DID 			INT NOT NULL references Directors(DID),
+		MPAA_Num		INT NOT NULL references Movies(MPAA_Num),
+		PRIMARY KEY (DID, MPAA_Num)
+);
+
+--Welcome to the ugliest query ever, of all time. 
+SELECT 	*
+FROM 	Directors
+WHERE 	DID in 
+	SELECT 	DID 
+	FROM 	Directed
+	WHERE 	MPAA_Num IN
+		SELECT 	MPAA_Num
+		FROM 	Movies
+		WHERE 	MPAA_Num IN
+			SELECT 	MPAA_NUM 
+			FROM 	Acts_In
+			WHERE 	AID IN
+				SELECT 	AID
+				FROM 	Actors
+				WHERE 	PID IN
+					SELECT 	PID
+					FROM 	People
+					WHERE 	First_Name = 'Sean' AND Last_Name = 'Connery';
